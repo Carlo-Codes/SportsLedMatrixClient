cbf55e1903621d1d5a7425b884747c6724781229
carlo <carlo.scrocca@googlemail.com>
2024-02-20 16:45:19 +0000

    getting websocket implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,36 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import './App.css'
+
+const serverAddress = '192.168.1.250:3000'
+const HTTPserverURL = 'http://' + serverAddress
+const wsServerURL = 'ws://' + serverAddress
 
 function App() {
 
   const [textToSend, setTextToSend] = useState('')
+  const [clientSentText, setClientSentText] = useState('')
 
   const handleTextChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
     setTextToSend(e.target.value)
   }
 
   async function SendData() {
-    const res = await fetch('http://192.168.1.250:3000/sendText', {
+    const res = await fetch(HTTPserverURL + '/sendText', {
         method: 'POST',
         body: textToSend
-    });
-
+    })
     return res
   } 
+
+  useEffect(() => {
+    const socket = new WebSocket(wsServerURL)
+    socket.addEventListener('message', function (e){
+      setClientSentText(e.data)
+    })
+    return () => {
+      socket.close()
+    }
+  },[])
 
   return(
     <div id ='mainUI' className="mainUI">
@@ -25,6 +39,10 @@ function App() {
       <label htmlFor="rawTextInput">Text to show</label>
       <input id="rawTextInput"  className='rawTextinput' onChange={handleTextChange}></input></div>
       <button id="sendButton" onClick={SendData}> Send</button>
+      <div className="matrixDisplay">
+        <div className="display Title">Currently Displayed</div>
+        <div className="diplayMessage">{clientSentText}</div>
+      </div>
     </div>
   )
 
