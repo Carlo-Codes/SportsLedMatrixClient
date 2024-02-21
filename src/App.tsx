c5aa51eec21c33bbc898269a2841dd50e6aada39
carlo <carlo.scrocca@googlemail.com>
2024-02-21 13:51:32 +0000

    modularising and adding football

--- src/App.tsx
+++ src/App.tsx
@@ -1,51 +1,11 @@
-import { useEffect, useState } from "react";
-import './App.css'
 
-const serverAddress = '192.168.1.250:3000'
-const HTTPserverURL = 'http://' + serverAddress
-const wsServerURL = 'ws://' + serverAddress
+import './App.css'
+import MatrixUi from './matrixUI'
 
 function App() {
-
-  const [textToSend, setTextToSend] = useState('')
-  const [clientSentText, setClientSentText] = useState('')
-
-  const handleTextChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
-    setTextToSend(e.target.value)
-  }
-
-  async function SendData() {
-    const res = await fetch(HTTPserverURL + '/sendText', {
-        method: 'POST',
-        body: textToSend
-    })
-    return res
-  } 
-
-  useEffect(() => {
-    const socket = new WebSocket(wsServerURL)
-    socket.addEventListener('message', function (e){
-      setClientSentText(e.data)
-    })
-    return () => {
-      socket.close()
-    }
-  },[])
-
   return(
-    <div id ='mainUI' className="mainUI">
-    <div id='Title' className="Title">LED Matrix Client</div>
-    <div id="rawTextInputDiv" className="textInput">
-      <label htmlFor="rawTextInput">Text to show</label>
-      <input id="rawTextInput"  className='rawTextinput' onChange={handleTextChange}></input></div>
-      <button id="sendButton" onClick={SendData}> Send</button>
-      <div className="matrixDisplay">
-        <div className="display Title">Currently Displayed</div>
-        <div className="diplayMessage">{clientSentText}</div>
-      </div>
-    </div>
+    <MatrixUi route='/matrix1'></MatrixUi>
   )
-
 }
 
 export default App
---
+++ src/matrixUI.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+import './App.css'
+
+const serverAddress = '192.168.1.250:3000'
+const HTTPserverURL = 'http://' + serverAddress
+const wsServerURL = 'ws://' + serverAddress
+
+function MatrixUi(props:{route:string}) {
+
+  const [textToSend, setTextToSend] = useState('')
+  const [clientSentText, setClientSentText] = useState('')
+
+  const handleTextChange = (e:React.ChangeEvent<HTMLInputElement>)=>{
+    setTextToSend(e.target.value)
+  }
+
+  async function SendText() {
+    const res = await fetch(HTTPserverURL + props.route +'/sendText', {
+        method: 'POST',
+        body: textToSend
+    })
+    return res
+  }
+
+  async function sendFootball(){
+    const res = await fetch(HTTPserverURL + props.route +'/football', {
+        method: 'GET',
+    })
+    return res
+  }
+  
+
+
+
+  useEffect(() => {
+    const socket = new WebSocket(wsServerURL + props.route)
+    socket.addEventListener('message', function (e){
+      setClientSentText(e.data)
+    })
+    return () => {
+      socket.close()
+    }
+  },[])
+
+  return(
+    <div id ='mainUI' className="mainUI">
+    <div id='Title' className="Title">LED Matrix Client</div>
+    <div id="rawTextInputDiv" className="textInput">
+      <label htmlFor="rawTextInput">Text to show</label>
+      <input id="rawTextInput"  className='rawTextinput' onChange={handleTextChange}></input></div>
+      <button id="sendButton" onClick={SendText}> Send</button>
+      <button id="footballButton" onClick={sendFootball}>Football</button>
+      <div className="matrixDisplay">
+        <div className="display Title">Currently Displayed</div>
+        <div className="diplayMessage">{clientSentText}</div>
+      </div>
+    </div>
+  )
+
+}
+
+export default MatrixUi
